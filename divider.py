ad5892e50b91d765debb76e7e414342d57f87f5d
amogus-spectator <177993284+amogus-spectator@users.noreply.github.com>
2025-01-05 16:44:39 +0700

    Tried Cython to compile the code faster

--- divider.py
+++ divider.py
@@ -1,5 +1,5 @@
 from random import *
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 
 agent_ids = []
 class Agent:
@@ -39,34 +39,35 @@ class Randomizer:
 def generation(agents_generator):
     def div(agents_generator_list, divisor):
         agent_partitions = []
-        processing_list = agents_generator_list
-        for i in range(len(agents_generator_list)//divisor):
-            partition = [processing_list[i]]
-            randomizer = Randomizer(0, len(processing_list)-1)
-            while processing_list[randomizer.randomize()] not in partition:
-                partition.append(processing_list[randomizer.randomize()])
+        processing_list = agents_generator_list[:]
+        for i in range(len(agents_generator_list) // divisor):
+            if not processing_list:
+                break
+            partition = [processing_list.pop(0)]
+            randomizer = Randomizer(0, len(processing_list) - 1)
+            while len(partition) < divisor and processing_list:
+                random_index = randomizer.randomize()
+                if random_index < len(processing_list):
+                    partition.append(processing_list.pop(random_index))
             agent_partitions.append(partition)
-            processing_list = [x for x in processing_list if x not in partition]
         if processing_list:
             agent_partitions.append(processing_list)
         return agent_partitions
-    
+
     agent_pairs = div(agents_generator, 2)
-    
+
     def comparator(agent1, agent2):
         agent1.generate_new_value(1, 100)
         agent2.generate_new_value(1, 100)
         if agent1.get_value() > agent2.get_value():
             return agent1
-        elif agent1.get_value() < agent2.get_value():
+        else:
             return agent2
-        elif agent1.get_value() == agent2.get_value():
-            return 1
         
     def comparartor_bin(agent1, agent2):
         r = comparator(agent1, agent2)
         if r == 1:
-            return 1
+            return 0
         elif r == agent1:
             return 0
         elif r == agent2:
@@ -84,13 +85,18 @@ def generation(agents_generator):
             
             
     simulator_result = []
+    simulator_result_bin = []
     while len(agent_pairs) > 1:
         if len(agent_pairs[0]) == 1:
             simulator_result.append(agent_pairs[0][0].get_id())
+            simulator_result_bin.append(0)
             agent_pairs = agent_pairs[1:]
         simulator_result.append(simulate(agent_pairs[0]))
+        simulator_result_bin.append(comparartor_bin(agent_pairs[0][0], agent_pairs[0][1]))
         agent_pairs = agent_pairs[1:]
-    return simulator_result
+        
+    simulator_appended_result = [simulator_result, simulator_result_bin]
+    return simulator_appended_result
 def main():
     def generate_agents(num_agents, value_range):
         agents = []
@@ -100,7 +106,20 @@ def main():
             agents.append(agent)
         return agents
     
-    r = generate_agents(64, [1, 100])
+    r = generate_agents(4, [1, 100])
+    simul_r = []
+    while len(r) > 1:
+        bypasser = generation(r)
+        r = bypasser[0]
+        simul_r.append(bypasser[1])
+        print(r)
+    
+    i = 1
+    while i < len(simul_r):
+        plt.plot(simul_r[i-1], label=f"Generation {i}")
+        i += 1
+    plt.legend()
+    plt.show()
     
     
 
